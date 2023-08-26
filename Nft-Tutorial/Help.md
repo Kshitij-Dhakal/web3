2a21b30c5479420b8671b401f1496c9ce5bac858
Kshitij Dhakal <dhakalkshitij@gmail.com>
2023-08-26 14:21:36 -0400

    creating basic nft implementation

---
+++ Nft-Tutorial/Help.md
@@ -0,0 +1,14 @@
+# Nft-Tutorial
+
+## Project Initialization
+
+```shell
+npm init --yes
+npm install --save-dev hardhat @nomicfoundation/hardhat-toolbox
+```
+
+Setup hardhat project using
+
+```shell
+npx hardhat
+```
---
+++ Nft-Tutorial/hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    // Define the sepolia network parameters here
+    sepolia: {
+      url: process.env.QUICKNODE_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
